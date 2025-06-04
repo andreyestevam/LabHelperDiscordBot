d4dcf2a5577ecea7ce06389a99728986d2dce9c0
Andrey Estevam Seabra <profissional.drey@gmail.com>
2025-06-04 17:06:20 -0400

    Refactor Resources cog to initialize database connection within the class and improve resource management commands

--- cogs/resources.py
+++ cogs/resources.py
@@ -2,15 +2,36 @@ import discord
 from discord.ext import commands
 import sqlite3
 
-connection = sqlite3.connect('resources.db')
-cursor = connection.cursor()
+class Resources(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.init_db()
+    
+    def init_db(self):
+        with sqlite3.connect('resources.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS resources (
+                        resource_title TEXT NOT NULL,
+                        description TEXT,
+                        link TEXT NOT NULL
+                    )         
+                """)
+    
+    @commands.command()
+    async def resources(self, ctx):
+        pass
 
-cursor.execute("""
-                CREATE TABLE resources (
-                    resource_title TEXT NOT NULL,
-                    description TEXT,
-                    link TEXT NOT NULL
-                )         
-            """)
+    @commands.command()
+    async def add_resource(self, ctx):
+        pass
 
-connection.close()
+    @commands.command()
+    async def delete_resource(self, ctx):
+        pass
+
+async def setup(bot):
+        """
+        Required setup function for the Cog to be loaded.
+        """
+        await bot.add_cog(Resources(bot))
