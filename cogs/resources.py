d77214ede1e52209a349a30bd6bb59bd7f4bd0e3
Andrey Estevam Seabra <profissional.drey@gmail.com>
2025-06-04 16:57:04 -0400

    Initial SQLite table steps.

--- cogs/resources.py
+++ cogs/resources.py
@@ -3,7 +3,14 @@ from discord.ext import commands
 import sqlite3
 
 connection = sqlite3.connect('resources.db')
+cursor = connection.cursor()
 
-
+cursor.execute("""
+                CREATE TABLE resources (
+                    resource_title TEXT NOT NULL,
+                    description TEXT,
+                    link TEXT NOT NULL
+                )         
+            """)
 
 connection.close()
